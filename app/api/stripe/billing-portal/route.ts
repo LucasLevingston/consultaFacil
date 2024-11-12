e26a096f200899fe5886cb6be326135a050144e6
LucasLevingston <lucaolevingston@outlook.com>
2024-11-11 22:40:38 -0300

    error handler

--- app/api/stripe/billing-portal/route.ts
+++ app/api/stripe/billing-portal/route.ts
@@ -1,20 +1,30 @@
+"use server";
+
 import { auth } from "@/auth";
 import stripe from "@/lib/stripe";
 import { NextRequest, NextResponse } from "next/server";
 
-const handler = async (req: NextRequest, res: NextResponse) => {
-  const sessionAuth = await auth();
+const handler = async (req: NextRequest) => {
+  try {
+    // Autenticação do usuário
+    const sessionAuth = await auth();
 
-  //   const {
-  //     data:{stripe_customer}
-  //   } = await supabase.from("profie")
-  // }
+    if (!sessionAuth || !sessionAuth.user || !sessionAuth.user.id) {
+      return NextResponse.json({ error: "User not authenticated." }, { status: 401 });
+    }
 
-  const session = await stripe.billingPortal.sessions.create({
-    customer: sessionAuth?.user?.id as string,
-    return_url: process.env.FRONT_URL,
-  });
-  res.send({
-    url: session.url,
-  });
+    // Criar uma sessão do Stripe Billing Portal
+    const session = await stripe.billingPortal.sessions.create({
+      customer: sessionAuth.user.id as string,
+      return_url: process.env.FRONT_URL,
+    });
+
+    // Retornar a URL da sessão criada
+    return NextResponse.json({ url: session.url });
+  } catch (error) {
+    console.error("Error creating billing portal session:", error);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
 };
+
+export { handler };
